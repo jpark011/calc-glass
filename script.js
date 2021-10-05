94bbb88e6ceb67940c390770c05cced1243133f4
jay(박현재) <jay.now@kakaocorp.com>
2021-10-06 01:41:47 +0900

    add whale

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const NUMS = new Array(10).fill(0).map((_, i) => i);
+const OPS = ['.', 'AC', 'Del', ''];
+
 const ans = document.querySelector('#ans');
 const prev = document.querySelector('#prev');
 
@@ -8,17 +11,22 @@ function onClickBtn({ target }) {
     return;
   }
 
+  if (char === 'AC') {
+    ans.value = '';
+    return;
+  }
+
+  if (char === 'Del') {
+    ans.value = ans.value.substring(0, ans.value.length - 1);
+    return;
+  }
+
   if (char === '=') {
     if (!ans.value) {
       return;
     }
     prev.value = ans.value;
     ans.value = eval(ans.value);
-    return;
-  }
-
-  if (char === 'AC') {
-    ans.value = '';
     return;
   }
 
