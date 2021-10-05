b7d0c2c54152f149b827c10fa22f39fcf0d1f2ac
jay(박현재) <jay.now@kakaocorp.com>
2021-10-06 00:12:22 +0900

    prototype

---
+++ script.js
@@ -0,0 +1,26 @@
+const ans = document.querySelector('#ans');
+const prev = document.querySelector('#prev');
+
+function onClickBtn({ target }) {
+  const { char } = target.dataset;
+
+  if (!char) {
+    return;
+  }
+
+  if (char === '=') {
+    if (!ans.value) {
+      return;
+    }
+    prev.value = ans.value;
+    ans.value = eval(ans.value);
+    return;
+  }
+
+  if (char === 'C') {
+    ans.value = '';
+    return;
+  }
+
+  ans.value += char;
+}
